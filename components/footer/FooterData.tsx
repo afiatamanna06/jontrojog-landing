08bab1d54a5820dc43db830a7d3725839a950b6d
afiatamanna06 <afiatamanna06@gmail.com>
2023-07-15 11:29:55 +0600

    Add footer social links

---
+++ components/footer/FooterData.tsx
@@ -0,0 +1,55 @@
+import { FiLinkedin, FiTwitter, FiInstagram, FiFacebook } from "react-icons/fi"
+
+export const footerLinks = [
+  {
+    name: 'About Us',
+    href: '/about',
+  },
+  {
+    name: 'Service',
+    href: '/service',
+  },
+  {
+    name: 'Our Works',
+    href: '/works',
+  },
+  {
+    name: 'Our Teams',
+    href: '/teams',
+  },
+  {
+    name: 'Pricing',
+    href: '/',
+  },
+  {
+    name: 'Documentation',
+    href: '/',
+  },
+  {
+    name: 'Guides',
+    href: '/',
+  },
+  {
+    name: 'API Status',
+    href: '/',
+  },
+]
+
+export const socialLinks = [
+    {
+        Icon: <FiFacebook />,
+        path: "https://www.facebook.com/",
+    },
+    {
+        Icon: <FiTwitter />,
+        path: "https://twitter.com/",
+    },
+    {
+        Icon: <FiInstagram />,
+        path: "https://www.instagram.com/",
+    },
+    {
+        Icon: <FiLinkedin />,
+        path: "https://www.linkedin.com/",
+    },
+]
