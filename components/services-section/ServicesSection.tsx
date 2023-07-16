e7d791fb612f3537386106f439caaea9d474b703
afiatamanna06 <afiatamanna06@gmail.com>
2023-07-17 01:02:01 +0600

    Update services card padding

--- components/services-section/ServicesSection.tsx
+++ components/services-section/ServicesSection.tsx
@@ -16,14 +16,14 @@ const ServicesSection = () => {
       <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
         <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-8">
           {servicesData.map((feature) => (
-            <div key={feature.name} className="relative pl-16 bg-[#00000015] dark:bg-[#ffffff15] py-4 pr-4 rounded-xl">
-              <dt className="text-base font-semibold leading-10 tracking-wider">
+            <div key={feature.name} className="relative pl-20 bg-[#00000012] dark:bg-[#ffffff15] py-4 pr-4 rounded-xl">
+              <dt className="text-base font-semibold leading-7 tracking-wider">
                 <div className="absolute left-4 top-4 flex h-10 w-10 items-center justify-center rounded-lg bg-black dark:bg-[#ffffffff] text-white dark:text-black">
                   {feature.icon}
                 </div>
                 {feature.name}
               </dt>
-              <dd className="mt-2 text-base leading-10 text-gray-500 dark:text-gray-400">
+              <dd className="mt-2 text-base leading-7 text-gray-500 dark:text-gray-400">
                 {feature.description}
               </dd>
             </div>
