9441b75702cbcbbd1e94bd57c984798c79b33417
afiatamanna06 <afiatamanna06@gmail.com>
2023-07-17 00:46:35 +0600

    Update service cards

--- components/about-section/About.tsx
+++ components/about-section/About.tsx
@@ -7,9 +7,9 @@ const stats = [
 
 export default function About() {
   return (
-    <div id="about" className="relative mx-auto max-w-7xl px-6 lg:px-8 pt-28 mb-12 lg:mb-40">
+    <div id="about" className="relative mx-auto max-w-7xl px-6 lg:px-8 mb-12 lg:mb-40">
       <img
-        className="w-full bg-gray-50 object-cover rounded-xl lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2"
+        className="w-full bg-gray-50 object-cover rounded-xl pt-28 lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2"
         src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80"
         alt=""
       />
